5570d6b0df107f3eefcfa8e93c5923e241a57e62
최현 <a6698ab@gmail.com>
2024-04-23 22:59:25 +0900

    a

--- components/header.jsx
+++ components/header.jsx
@@ -16,7 +16,7 @@ export function Header() {
     return (
         <nav className="flex flex-wrap items-center gap-4 pt-6 pb-12 sm:pt-12 md:pb-24">
             <Link href="/">
-                <Image src={netlifyLogo} alt="Netlify logo" />
+                <Image src={netlifyLogo} alt="Pear Tree Logo" />
             </Link>
         </nav>
     );
