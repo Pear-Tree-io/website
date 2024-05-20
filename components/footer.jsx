a3dfa48e62e28ee6ceb6f722ea09c6f8ee312d62
최현 <a6698ab@gmail.com>
2024-05-20 11:38:01 +0900

    a

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export function Footer() {
     return (
-        <footer className="antialiased text-orange-400">
-                {/* <h2>Contant Us</h2> */}
-                <p>ContactUs@pear-tree.io</p>
+        <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
+            <p className="text-sm">
+                <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="underline transition decoration-dashed text-primary underline-offset-8 hover:opacity-80">
+                    Next.js on Netlify
+                </Link>
+            </p>
         </footer>
     );
 };
