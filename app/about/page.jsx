0fa4fc3892e03259c50861a9170b3dbf6e28fccc
최현 <a6698ab@gmail.com>
2024-06-01 05:13:59 +0900

    a

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -16,7 +16,7 @@ export default async function Page() {
     return (
         <main>
         <h1>About</h1>
-        <p>Our company, established on May 9, 2024, is growing with a vision to turn imagination into reality through the fusion of technology and ideas. As we grow, the most important aspect we consider is remembering and valuing all our experiences. Currently, we are developing a game called 'Count Sheep,' which will become a solid stepping stone for our future.</p>
+        <p>Our company, established on May 9, 2024, is growing with a vision to turn imagination into reality through the fusion of technology and ideas. As we grow, the most important aspect we consider is remembering and valuing all our experiences. Currently, we are developing a game called &apos;Count Sheep&apos; which will become a solid stepping stone for our future.</p>
         </main>
     );
 }
