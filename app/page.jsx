4da8b55e8ab040fd30db92fad9bbac20e878a8ba
최현 <a6698ab@gmail.com>
2024-08-13 10:51:07 +0900

    a

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,6 @@ import { getNetlifyContext } from 'utils';
 import gameLogo from 'public/GetItOnGooglePlay_Badge_Web_color_English.png';
 import googlePlay from 'public/GetItOnGooglePlay_Badge_Web_color_English.png';
 import Image from 'next/image';
-import Link from 'next/link';
 
 const cards = [
     //{ text: 'Hello', linkText: 'someLink', href: '/' }
